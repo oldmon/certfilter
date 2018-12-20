d27c21414740b1d751e16d6b0580f16e3f33a091
oldmon <oldmonou@gmail.com>
2018-12-20 11:40:29 +0800

    Fix the check connection part

--- certfilter.py
+++ certfilter.py
@@ -2,51 +2,57 @@
 import sys,ssl,socket,urllib.request,validators
 from openpyxl import load_workbook
 from OpenSSL import crypto
-#please `pip install pyopenssl openpyxl validators` before running
+# Please `pip install pyopenssl openpyxl validators` before running
 
-#Global variables as settings
+# Global variables as settings
 GCIS_URL='http://data.gcis.nat.gov.tw/od/data/api/6BBA2268-1367-4B42-9CCA-BC17499EBE8C'
 evoidfile='evoid'
 timeout=4
 socket.setdefaulttimeout(timeout)
 
-#Print Usage
+# Print Usage
 if len(sys.argv)<2:
 	print('Usage: certfilter.py inputfile.xlsx')
 	sys.exit(1)
 
-#prevent empty extraction of cert element
+# Prevent empty extraction of cert element
 def xstr(s):
 	if s is None:
 		return ''
 	return str(s)
 
-#simeple way to check if host accept TLS connection
-def isSiteAlive(hostname):
-	try:
-		urllib.request.urlopen('https://'+hostname)
-		return True
-	except urllib.error.URLError as e:
-		print(e.reason)
+# Simeple way to check if host accept TLS connection
+def checkAlive(host='hicloud.hinet.net',port=443,timeout=3):
+	socket.setdefaulttimeout(timeout)
+	if validators.url('https://'+xstr(host)):
+		try:
+			socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+			return True
+		except Exception as ex:
+			print(ex.message)
+			return False
+	else:
 		return False
 
-#prepare set for EVSSL oids
+# Fetch peer certificate from Internet by ssl module
+
+# Prepare set for EVSSL oids
 with open(evoidfile)as f:
 	evoid=set(f.read().splitlines())
 
 f.close()
 
-#worksheet preparation
+# Worksheet preparation
 input_file=sys.argv[1]
 in_wb=load_workbook(filename=input_file)
 in_ws=in_wb[in_wb.sheetnames[0]]
 
-#worker loop
-for row in range(1,in_ws.max_row+1):
+# Worker loop
+for row in range(2,in_ws.max_row+1):
 	sub_c=sub_o=sub_ou=sub_cn=iss_c=iss_o=iss_cn=iss_ou=nota=notb=None
 	host=in_ws['A'+str(row)].value
 	weight=in_ws['B'+str(row)].value
-	if validators.url('https://'+xstr(host)) and isSiteAlive(xstr(host)):
+	if checkAlive(xstr(host)):
 		getcert=ssl.get_server_certificate((host,443))
 		workcert=crypto.load_certificate(crypto.FILETYPE_PEM,getcert)
 		sub_c=workcert.get_subject().C
@@ -59,4 +65,4 @@ for row in range(1,in_ws.max_row+1):
 		iss_cn=workcert.get_issuer().CN
 		notb=workcert.get_notBefore().decode('utf-8')
 		nota=workcert.get_notAfter().decode('utf-8')
-		print(str(row)+'\t'+xstr(host)+'\t'+xstr(sub_c)+'\t'+xstr(sub_o)+'\t'+xstr(sub_ou)+'\t'+xstr(sub_cn)+'\t'+xstr(iss_c)+'\t'+xstr(iss_o)+'\t'+xstr(iss_ou)+'\t'+xstr(iss_cn)+'\t'+xstr(notb)+'\t'+xstr(nota))
+		print(str(row)+','+xstr(host)+','+xstr(sub_c)+','+xstr(sub_o)+','+xstr(sub_ou)+','+xstr(sub_cn)+','+xstr(iss_c)+','+xstr(iss_o)+','+xstr(iss_ou)+','+xstr(iss_cn)+','+xstr(notb)+','+xstr(nota))
