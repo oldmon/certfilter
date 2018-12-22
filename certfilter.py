5f27a1c47c26159d0d7b1b0ec2ebe89fc72cb4ce
Nicholas Ou <oldmonou@gmail.com>
2018-12-22 17:03:42 +0800

    add blacklist and rearrange

--- certfilter.py
+++ certfilter.py
@@ -10,6 +10,7 @@ from openpyxl import load_workbook
 # Global variables as settings
 GCIS_URL = 'http://data.gcis.nat.gov.tw/od/data/api/6BBA2268-1367-4B42-9CCA-BC17499EBE8C'
 evoidfile = 'evoid'
+blacklistf = 'blacklist'
 TIMEOUT = 4
 socket.setdefaulttimeout(TIMEOUT)
 #headers = {"User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 ("
@@ -20,6 +21,16 @@ if len(sys.argv) < 2:
 	print('Usage: certfilter.py inputfile.xlsx')
 	sys.exit(1)
 
+# Prepare set for EVSSL oids
+with open(evoidfile)as f:
+	evoid = set(f.read().splitlines())
+
+
+with open(blacklistf)as f:
+	blacklist = set(f.read().splitlines())
+
+
+f.close()
 
 # Prevent empty extraction of cert element
 def xstr(s):
@@ -29,9 +40,11 @@ def xstr(s):
 
 
 # Simple way to check if host accept TLS connection
-def checkalive(host='hicloud.hinet.net', port=443, timeout=TIMEOUT):
+def isalive(host='hicloud.hinet.net', port=443, timeout=TIMEOUT):
 	socket.setdefaulttimeout(timeout)
-	if validators.url('https://' + xstr(host)):
+	if host in blacklist:
+		return False
+	elif validators.url('https://' + xstr(host)):
 		try:
 			socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
 			return True
@@ -84,13 +97,6 @@ def new_HTTPSConnection_connect(self):
 
 HTTPSConnection.connect = new_HTTPSConnection_connect
 
-# Prepare set for EVSSL oids
-with open(evoidfile)as f:
-	evoid = set(f.read().splitlines())
-
-
-f.close()
-
 # Worksheet preparation
 input_file = sys.argv[1]
 in_wb = load_workbook(filename=input_file)
@@ -101,7 +107,7 @@ for row in range(10, in_ws.max_row + 1):
 	sub_c = sub_o = sub_ou = sub_cn = iss_c = iss_o = iss_cn = iss_ou = nota = notb = None
 	host = in_ws['A' + str(row)].value
 	weight = in_ws['B' + str(row)].value
-	if checkalive(xstr(host)):
+	if isalive(xstr(host)):
 		print(xstr(host))
 		try:
 			r = requests.get('https://' + host, timeout=TIMEOUT)
